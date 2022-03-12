166d2b9787f3a2a6836c1589f5c91afdcf1e92b3
Leemhyungyu <lhglbkksj@naver.com>
2022-03-12 23:59:13 +0900

    Create Baekjoon1406.py
    백준 1406번 문제: 자료구조

---
+++ Data Structuures/Baekjoon1406.py
@@ -0,0 +1,20 @@
+# 백준 1406번 문제: 에디터(자료구조)
+import sys
+
+stack_l = list(input())
+stack_r = []
+n = int(input())
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == "L" and stack_l:
+        stack_r.append(stack_l.pop())
+    elif command[0] == "D" and stack_r:
+        stack_l.append(stack_r.pop())
+    elif command[0] == "B" and stack_l:
+        stack_l.pop()
+    elif command[0] == "P":
+        stack_l.append(command[1])
+
+print("".join(stack_l + list(reversed(stack_r))))
