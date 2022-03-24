5b1bf2af9d8ac9ebefbce6673bcde461e2d6c4b3
Leemhyungyu <lhglbkksj@naver.com>
2022-03-24 18:18:43 +0900

    Create Baekjoon16194.py
    백준 16194번 문제: 다이나믹 프로그래밍

---
+++ Dynamic Programming/Baekjoon16194.py
@@ -0,0 +1,16 @@
+# 백준 16194번 문제: 카드 구매하기2 (다이나믹 프로그래밍)
+
+n = int(input())
+array = list(map(int, input().split()))
+
+d = [0 for _ in range(n + 1)]
+
+d[1] = array[0]
+
+for i in range(1, n + 1):
+  d[i] = array[i - 1]
+  for j in range(1, i):
+    d[i] = min(d[i], array[(i - j) - 1] + d[j])
+  
+
+print(d[n])
