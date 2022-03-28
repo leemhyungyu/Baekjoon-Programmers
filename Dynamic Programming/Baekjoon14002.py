71dc49800e269c13bb41eb6efa27274420cd372d
Leemhyungyu <lhglbkksj@naver.com>
2022-03-29 01:35:23 +0900

    Update Baekjoon14002.py
    틀려서 다시 품

--- Dynamic Programming/Baekjoon14002.py
+++ Dynamic Programming/Baekjoon14002.py
@@ -1,10 +1,11 @@
-# 백준 14002번 문졔: 가장 긴 증가하는 부분 수열4 (다이나믹 프로그래밍)
+## 백준 14002번 문졔: 가장 긴 증가하는 부분 수열4 (다이나믹 프로그래밍)
 n = int(input())
 
 array = list(map(int, input().split()))
 
 d = [0] * (n + 1)
 result = []
+index = 0
 
 for i in range(n):
   for j in range(i):
@@ -14,9 +15,19 @@ for i in range(n):
 
 print(max(d))
 
-for i in range(1, len(d) + 1): 
- for j in range(len(d)):
-   if d[j] == i and array[j] not in result:
-     result.append(array[j])
-     
+for i in range(len(d)):
+  if d[i] == max(d):
+    index = i
+
+for i in range(max(d), 0, -1):
+  for j in range(index, -1, -1):
+    if d[j] == i:
+      result.append(array[j])
+      index -= 1  
+      break
+    else:
+      index -= 1
+
+  
+result.sort(reverse = False)
 print(*result)
