46a77c19bf91aad54af2f69b0a149d37d8a1e20d
leemhyungyu <lhglbkksj@naver.com>
2022-10-01 01:37:50 +0900

    [실버 5] - 파일 합치기

---
+++ Sorting/Baekjoon11728.swift
@@ -0,0 +1,69 @@
+//
+//  Baekjoon11728.swift
+//  
+//
+//  Created by 임현규 on 2022/10/01.
+//
+
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let fIO = FileIO()
+
+let n = fIO.readInt()
+let m = fIO.readInt()
+
+var array = [Int]()
+
+for _ in 0...(n + m - 1) {
+  array.append(fIO.readInt())
+}
+
+print(array.sorted().map { String($0) }.joined(separator: " "))
