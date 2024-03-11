1e67be52750e2ba7c344fb342703c2ae7c093133
leemhyungyu <lhglbkksj@naver.com>
2024-03-12 00:12:20 +0900

    [Lv3] 네트워크

--- Programmers/Lv3/네트워크.swift
+++ Programmers/Lv3/네트워크.swift
@@ -48,3 +48,44 @@ func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     
     return count
 }
+
+func solution(_ n:Int, _ computers:[[Int]]) -> Int {
+    var result = 0
+    var visited = Array(repeating: false, count: n)
+
+    func bfs(_ start: [(Int, Int)]) {
+        var queue = start
+        
+        while !queue.isEmpty {
+        var pos = queue.removeLast()
+        let i = pos.0, j = pos.1
+   
+            visited[i] = true
+            visited[j] = true
+            
+            for ni in 0..<n {
+                for nj in 0..<n {
+                    if i == nj || ni == j { continue }
+                    if computers[i][nj] == 1 && !visited[nj] {
+                        queue.append((i, nj))
+                    }
+                    
+                    if computers[ni][j] == 1 && !visited[ni] {
+                        queue.append((ni, j))
+                    }
+                }
+            }
+        }
+    }
+        
+    for i in 0..<n {
+        for j in 0..<n {
+            if !visited[i] {
+                bfs([(i, j)])
+                result += 1
+            }
+        }
+    }
+    
+    return result
+}
