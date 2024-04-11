b8515467e38ebbfdc903c99b0de3ac13616b0128
leemhyungyu <lhglbkksj@naver.com>
2024-04-11 17:31:47 +0900

    [골드 4] 특정한 최단 경로

--- Baekjoon/Dijkstra's/Baekjoon1504.py
+++ Baekjoon/Dijkstra's/Baekjoon1504.py
@@ -1,57 +1,44 @@
-import heapq
-import sys
+from collections import defaultdict
+import sys, heapq
+
 input = sys.stdin.readline
-INF = int(1e9)
-
 n, e = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-
-# 다익스트라 알고리즘 
-def dijkstra(start, end):
-  
-  distance = [INF] * (n + 1)
-  
-  q = []
-  heapq.heappush(q, (0, start))
-  distance[start] = 0
-
-  while q:
-
-    dist, now = heapq.heappop(q)
-
-    if distance[now] < dist:
-      continue
-
-    if now == end:
-      return dist
-      
-    for i in graph[now]:
-      cost = dist + i[1]
-      if cost < distance[i[0]]:
-        distance[i[0]] = cost
-        heapq.heappush(q, (cost, i[0]))
-  
-for _ in range(e):
-  a, b, c = map(int, input().split())
-  graph[a].append((b, c))
-  graph[b].append((a, c))
-
+array = [list(map(int, input().split())) for _ in range(e) ]
 v1, v2 = map(int, input().split())
 
-# 간선의 개수가 0일 경우 -1 출력
-if e == 0:
-  print(-1)
-# 그렇지 않은 경우
-else:
-  # 1 -> v1 -> v2 -> n 까지의 거리 
-  result_1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, n)
-  # 1 -> v2 -> v1 -> n 까지의 거리
-  result_2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, n)
+def solution(array, n, e, v1, v2):
+    graph = defaultdict(dict)
 
-  # 두 경로가 INF보다 크면 -1 출력
-  if result_1 > INF and result_2 > INF:
-    print(-1)
-  # 그렇지 않은 경우 더 작은 값 출력
-  else:
-    print(min(result_1, result_2))
-  
+    for i in array:
+        graph[i[0]][i[1]] = i[2]
+        graph[i[1]][i[0]] = i[2]
+
+    if graph[v1] == {} or graph[v2] == {} or graph[n] == {} :
+        return -1
+    
+    oneCost = dijkstra(graph, 1)
+    v1Cost = dijkstra(graph, v1)
+    v2Cost = dijkstra(graph, v2)
+    
+    return min(oneCost[v1] + v1Cost[v2] + v2Cost[n], oneCost[v2] + v2Cost[v1] + v1Cost[n])
+
+def dijkstra(graph, start):
+    costs = {i: 1e9 for i in graph}
+    costs[start] = 0
+    queue = []
+    heapq.heappush(queue, [costs[start], start])
+
+    while queue:
+        nowCost, nowNode = heapq.heappop(queue)
+        if costs[nowNode] < nowCost:
+            continue
+        for newNode, newCost in graph[nowNode].items():
+            cost = newCost + nowCost
+            if cost < costs[newNode]:
+                costs[newNode] = cost
+                heapq.heappush(queue, [cost, newNode])
+    
+    return costs                               
+    
+print(solution(array, n, e, v1, v2))
+
